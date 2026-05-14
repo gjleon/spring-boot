3c508bba837b67546868aa0a6ceec80d91e0b521
gjleon <gabriel_leon23@hotmail.com>
2026-05-14 09:54:48 -0400

    adicionando projeto inicial

---
+++ anime-service/src/test/java/com/gjleon/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjleon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
